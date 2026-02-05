5a587aa8f7712914bc7469529af95ab11a164d36
Laszl0T0th <laszlot0th.wien@proton.me>
2026-02-05 11:43:04 +0000

    EmployeeManagement.Web

---
+++ EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,227 @@
+﻿using AutoMapper;
+using EmployeeManagement.Models;
+using EmployeeManagement.Web.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : ComponentBase
+    {
+        // EN
+        // Authentication and authorization state data in code in blazor
+        // how to obtain authentication and authorization state data in code in blazor
+        // Cascading AuthenticationState parameter
+        // Cascading AuthenticationState parameter() provides authentication and authorization state data.Task<AuthenticationState>
+        // If the user is not authenticated, the request is redirected to the page. login
+        // The return url is also passed as the query string parameter to the login page.
+        // Upon successful login, the user will be redirected to the page he was trying to access.
+        // GE
+        // Authentifizierungs- und Autorisierungszustandsdaten im Code in Blazor
+        // So erhalten Sie Authentifizierungs- und Autorisierungsstatusdaten im Code in Blazor
+        // Kaskadierender AuthenticationState-Parameter
+        // Der kaskadierende AuthenticationState-Parameter () stellt Daten zum Authentifizierungs- und Autorisierungsstatus bereit.Task<AuthenticationState>
+        // Wenn der Benutzer nicht authentifiziert ist, wird die Anfrage auf die Seite umgeleitet. Anmeldung
+        // Die Rückgabe-URL wird auch als Parameter der Abfragezeichenfolge an die Anmeldeseite übergeben.
+        // Nach erfolgreicher Anmeldung wird der Benutzer auf die Seite umgeleitet, auf die er zugreifen wollte.
+        // HU
+        // Szerep- vagy házirend-alapú engedélyezés
+        // hogyan szerezheti be a hitelesítési és engedélyezési állapotadatokat a blazor kódjában.
+        // Cascading AuthenticationState paraméter
+        // A Cascading AuthenticationState paraméter() hitelesítési és engedélyezési állapotadatokat biztosít.Task<AuthenticationState>
+        // Ha a felhasználó nincs hitelesítve, a rendszer átirányítja a kérést az oldalra.login
+        // A visszatérési URL-cím lekérdezési sztring paraméterként is át lesz adva a bejelentkezési oldalnak.
+        // Sikeres bejelentkezés után a felhasználó átirányításra kerül arra az oldalra, amelyhez megpróbált hozzáférni.
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
+        // EN
+        // Check if authenticated user satisfies a specific policy
+        //Task<AuthenticationState> can be combined with , to check if a specific aothorization policy is satisfied.IAuthorizationService
+        // GE
+        // Überprüfen Sie, ob der authentifizierte Benutzer eine bestimmte Richtlinie erfüllt
+        // Task<AuthenticationState> kann mit kombiniert werden, um zu prüfen, ob eine bestimmte Autorisierungsrichtlinie erfüllt ist.IAuthorizationService
+        // HU
+        // Ellenőrizze, hogy a hitelesített felhasználó megfelel-e egy adott szabályzatnak
+        //Task<AuthenticationState> kombinálható a -val, hogy ellenőrizze, hogy egy adott aothorizációs irányelv teljesül-e.IAuthorizationService
+        //public class EditEmployeeBase : ComponentBase
+        //        {
+        //            [CascadingParameter]
+        //            private Task<AuthenticationState> authenticationStateTask { get; set; }
+
+        //            [Inject]
+        //            private IAuthorizationService AuthorizationService { get; set; }
+
+        //            protected async override Task OnInitializedAsync()
+        //            {
+        //                var user = (await authenticationStateTask).User;
+
+        //                if ((await AuthorizationService.AuthorizeAsync(user, "admin-policy"))
+        //                .Succeeded)
+        //                {
+        //                    // Execute code specific to admin-policy
+        //                }
+        //            }
+        //        }
+        // - - - - -  END COMMENT  - - - - - - - - - - - - 
+
+
+
+
+
+
+        // EN
+        // retrieves the employee details by ID.
+        // GE
+        // Ruft die Mitarbeiterdetails nach ID ab.
+        // HU
+        // lekéri az alkalmazott adatait azonosító alapján.
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public string PageHeader { get; set; }
+
+        // EN
+        // Used to store the Rest API call, property contains the employee data we want to edit.
+        // GE
+        // Wird verwendet, um den Rest-API-Aufruf zu speichern, Eigenschaft enthält die Mitarbeiterdaten, die wir bearbeiten möchten.
+        // HU
+        // A Rest API hívás tárolására használjuk. A tulajdonság tartalmazza a szerkeszteni kívánt alkalmazotti adatokat.
+        private Employee Employee { get; set; } = new Employee();
+
+        public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
+
+        [Inject]
+        public IDepartmentService DepartmentService { get; set; }
+
+        public List<Department> Departments { get; set; } = new List<Department>();
+
+        //public string DepartmentId { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        [Inject]
+        public IMapper Mapper { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            var authenticationState = await authenticationStateTask;
+
+            if (!authenticationState.User.Identity.IsAuthenticated)
+            {
+                string returnUrl = WebUtility.UrlEncode($"/editEmployee/{Id}");
+                NavigationManager.NavigateTo($"/identity/account/login?returnUrl={returnUrl}");
+            }
+            // EN
+            // Check if authenticated user is in a specific role
+            // GE
+            // Prüfen, ob der authentifizierte Benutzer eine bestimmte Rolle hat
+            // HU
+            // Ellenőrizze, hogy a hitelesített felhasználó egy adott szerepkörben van-e
+            //if (authenticationState.User.IsInRole("Administrator"))
+            //{
+            //   Execute Admin logic
+            //}
+
+
+            // old code
+            //Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+
+            int.TryParse(Id, out int employeeId);
+
+            if (employeeId != 0)
+            {
+                PageHeader = "Edit Employee";
+                Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+            }
+            else
+            {
+                PageHeader = "Create Employee";
+                Employee = new Employee
+                {
+                    DepartmentId = 1,
+                    DateOfBrith = DateTime.Now,
+                    PhotoPath = "images/nophoto.jpg"
+                };
+            }
+
+            Departments = (await DepartmentService.GetDepartments()).ToList();
+
+            //DepartmentId = Employee.DepartmentId.ToString();
+
+
+            // NuGet Package: AutoMapper.Extensions.Microsoft.DependencyInjection
+            // EN
+            // It replaces this code.
+            // GE
+            // Er ersetzt diesen Code.
+            // HU
+            // Ezt a kódot helyettesíti.
+            //EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            //EditEmployeeModel.FirstName = Employee.FirstName;
+            //EditEmployeeModel.LastName = Employee.LastName;
+            //EditEmployeeModel.Email = Employee.Email;
+            //EditEmployeeModel.ConfirmEmail = Employee.Email;
+            //EditEmployeeModel.DateOfBrith = Employee.DateOfBrith;
+            //EditEmployeeModel.Gender = Employee.Gender;
+            //EditEmployeeModel.PhotoPath = Employee.PhotoPath;
+            //EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            //EditEmployeeModel.Department = Employee.Department;
+
+            Mapper.Map(Employee, EditEmployeeModel);
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            Mapper.Map(EditEmployeeModel, Employee);
+
+            //var result = await EmployeeService.UpdateEmployee(Employee);
+            //if (result != null)
+            //{
+            //    NavigationManager.NavigateTo("/");
+            //}
+
+            Employee result = null;
+
+            if (Employee.EmployeeId != 0)
+            {
+                result = await EmployeeService.UpdateEmployee(Employee);
+            }
+            else
+            {
+                result = await EmployeeService.CreateEmployee(Employee);
+            }
+            if (result != null)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+
+        protected BlazorProject.Components.ConfirmBase DeleteConfirmation { get; set; }
+
+        protected void Delete_Click()
+        {
+            DeleteConfirmation.Show();
+        }
+
+        protected async Task ConfirmDelete_Click(bool deleteConfirmed)
+        {
+            if (deleteConfirmed)
+            {
+                await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+                NavigationManager.NavigateTo("/");
+            }
+        }
+
+        //protected async Task Delete_Click()
+        //{
+        //    await EmployeeService.DeleteEmployee(Employee.EmployeeId);
+        //    NavigationManager.NavigateTo("/");
+        //}
+    }
+}
---
+++ EmployeeManagement.Web/Services/EmployeeService.cs
@@ -0,0 +1,182 @@
+﻿using EmployeeManagement.Models;
+using System.Text.Json;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace EmployeeManagement.Web.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        // EN
+        // We are using HttpClient class to call the REST API service.
+        // This class is in System.Net.Http namespace.
+        // HttpClient is injected into the EmployeeService using dependency injection.
+        // We have not registered HttpClient service with the dependency injection container yet.We will do that in just a bit.
+        // Pass the REST API endpoint(api/employees) to httpClient.GetJsonAsync method.
+        // GE
+        // Wir verwenden die HttpClient-Klasse, um den REST-API-Dienst aufzurufen.
+        // Diese Klasse befindet sich im System.Net.Http-Namespace.
+        // HttpClient wird mithilfe von Abhängigkeitsinjektion in den EmployeeService eingefügt.
+        // Wir haben den HttpClient-Dienst noch nicht mit dem Dependency-Injection-Container registriert. Das werden wir in Kürze tun.
+        // Übergeben Sie den REST-API-Endpunkt (api/employees) an die Methode httpClient.GetJsonAsync.
+        // HU
+        // A HttpClient osztályt használjuk a REST API hívásához.
+        // Ez az osztály a System.Net.Http névtérben található.
+        // A HttpClient függőséginjektálással injektálódik az EmployeeService szolgáltatásba.
+        // Még nem regisztráltuk a HttpClient szolgáltatást a függőséginjektálási tárolóval.Ezt egy kicsit meg fogjuk tenni.
+        // Adja át a REST API végpontot (api/employees) a httpClient.GetJsonAsync metódusnak.
+
+        // Nuget Package: Microsoft.AspNetCore.Blazor.HttpClient
+        // ASP.NET Core default HTTP feature implementations.
+        private readonly HttpClient httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0&fbclid=IwAR2svwvcl0NIVC0_Hvq4NfhlWsOMIm0f4YUrqnzbZD4oozYO_scT3fcUan0
+            //httpClient.DefaultRequestHeaders.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+            // Solution 1: using System.Text.Json;
+            //var response = await httpClient.GetAsync("api/employees");
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employees = JsonSerializer.Deserialize<Employee[]>(responseContent,
+            //        new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            //return employees;
+
+            // Solution 2: using Newtonsoft.Json;
+            var response = await httpClient.GetAsync("api/employees");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var employees = JsonConvert.DeserializeObject<Employee[]>(responseContent);
+            return employees;
+
+            // Solution 3: using System.Net.Http.Json;
+            //return await httpClient.GetFromJsonAsync<Employee[]>("api/employees");
+
+
+        }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0&fbclid=IwAR2svwvcl0NIVC0_Hvq4NfhlWsOMIm0f4YUrqnzbZD4oozYO_scT3fcUan0
+            // https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=net-8.0
+            // https://learn.microsoft.com/en-us/dotnet/api/system.net.http.httpresponsemessage?view=net-8.0
+            //httpClient.DefaultRequestHeaders.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+            // Solution 1: using System.Text.Json;
+            //var response = await httpClient.GetAsync("api/employees/{id}");
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employees = JsonSerializer.Deserialize<Employee>(responseContent,
+            //        new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            //return employees;
+
+            // Solution 2: using Newtonsoft.Json;
+            var response = await httpClient.GetAsync("api/employees/{id}");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var employees = JsonConvert.DeserializeObject<Employee>(responseContent);
+            return employees;
+
+            // Solution 3: using System.Net.Http.Json;
+            //return await httpClient.GetFromJsonAsync<Employee>($"api/employees/{id}");
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee updatedEmployee)
+        {
+            #region Comment
+            // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0&fbclid=IwAR2svwvcl0NIVC0_Hvq4NfhlWsOMIm0f4YUrqnzbZD4oozYO_scT3fcUan0
+            // EN
+            // Serializes the parameter to JSON using .TodoItemSystem.Text.Json
+            // Creates an instance of StringContent to package the serialized JSON for sending in the HTTP request's body.
+            // Calls PostAsync to send the JSON content to the specified URL.This is a relative URL that gets added to the HttpClient.BaseAddress.
+            // Calls EnsureSuccessStatusCode to throw an exception if the response status code doesn't indicate success.
+            // GE
+            // Serialisiert den Parameter mit .TodoItemSystem.Text.Json in JSON
+            // Erstellt eine Instanz von StringContent, um die serialisierte JSON-Datei zum Senden im Hauptteil der HTTP-Anforderung zu verpacken.
+            // Ruft PostAsync auf, um den JSON-Inhalt an die angegebene URL zu senden Dies ist eine relative URL, die zu HttpClient.BaseAddress hinzugefügt wird.
+            // Ruft CertainSuccessStatusCode auf, um eine Ausnahme auszulösen, wenn der Antwortstatuscode keinen Erfolg anzeigt.
+            // HU
+            // Szerializálja a paramétert JSON-ra a használatával.TodoItemSystem.Text.Json
+            // Létrehoz egy StringContent példányt a szerializált JSON csomagolásához a HTTP-kérés törzsében való küldéshez.
+            // Meghívja a PostAsync metódust, hogy elküldje a JSON - tartalmat a megadott URL - címre.Ez egy relatív URL - cím, amely hozzá lesz adva a HttpClient.BaseAddress címhez.
+            // Meghívja az EnsureSuccessStatusCode metódust, hogy kivételt dobjon, ha a válasz állapotkódja nem jelzi a sikert.
+            #endregion
+            // Solution 1: using System.Text.Json; PutAsJsonAsync
+            //var response = await httpClient.PutAsJsonAsync("api/employees", updatedEmployee);
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employee = JsonSerializer.Deserialize<Employee>(responseContent);
+            //return employee;
+
+            // Solution 2: using System.Text.Json; PutAsync
+            //var json = JsonSerializer.Serialize(updatedEmployee);
+            //var stringContent = new StringContent(json);
+            //var response = await httpClient.PutAsync("api/employees", stringContent);
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employee = JsonSerializer.Deserialize<Employee>(responseContent);
+            //return employee;
+
+            // Solution 3: using Newtonsoft.Json; PutAsync
+            var json = JsonConvert.SerializeObject(updatedEmployee);
+            var stringContent = new StringContent(json);
+            var response = await httpClient.PutAsync("api/employees", stringContent);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var employee = JsonConvert.DeserializeObject<Employee>(responseContent);
+            return employee;
+        }
+
+        public async Task<Employee> CreateEmployee(Employee newEmployee)
+        {
+            #region Comment
+            // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-6.0&fbclid=IwAR2svwvcl0NIVC0_Hvq4NfhlWsOMIm0f4YUrqnzbZD4oozYO_scT3fcUan0
+            // EN
+            // Serializes the parameter to JSON using .TodoItemSystem.Text.Json
+            // Creates an instance of StringContent to package the serialized JSON for sending in the HTTP request's body.
+            // Calls PostAsync to send the JSON content to the specified URL.This is a relative URL that gets added to the HttpClient.BaseAddress.
+            // Calls EnsureSuccessStatusCode to throw an exception if the response status code doesn't indicate success.
+            // GE
+            // Serialisiert den Parameter mit .TodoItemSystem.Text.Json in JSON
+            // Erstellt eine Instanz von StringContent, um die serialisierte JSON-Datei zum Senden im Hauptteil der HTTP-Anforderung zu verpacken.
+            // Ruft PostAsync auf, um den JSON-Inhalt an die angegebene URL zu senden Dies ist eine relative URL, die zu HttpClient.BaseAddress hinzugefügt wird.
+            // Ruft CertainSuccessStatusCode auf, um eine Ausnahme auszulösen, wenn der Antwortstatuscode keinen Erfolg anzeigt.
+            // HU
+            // Szerializálja a paramétert JSON-ra a használatával.TodoItemSystem.Text.Json
+            // Létrehoz egy StringContent példányt a szerializált JSON csomagolásához a HTTP-kérés törzsében való küldéshez.
+            // Meghívja a PostAsync metódust, hogy elküldje a JSON - tartalmat a megadott URL - címre.Ez egy relatív URL - cím, amely hozzá lesz adva a HttpClient.BaseAddress címhez.
+            // Meghívja az EnsureSuccessStatusCode metódust, hogy kivételt dobjon, ha a válasz állapotkódja nem jelzi a sikert.
+            //httpClient.DefaultRequestHeaders.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+            //https://stackoverflow.com/questions/70508587/status-400-from-webapi-when-calling-postasjsonasync-from-blazor-client
+            #endregion
+            // Solution 1: using System.Text.Json; PostAsJsonAsync
+            //var response = await httpClient.PostAsJsonAsync("api/employees", newEmployee);
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employee = JsonSerializer.Deserialize<Employee>(responseContent);
+            //return employee;
+
+            // Solution 2: using System.Text.Json; PostAsync
+            //var json = JsonSerializer.Serialize(newEmployee);
+            //var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            //var response = await httpClient.PostAsync("api/employees", stringContent);
+            //var responseContent = await response.Content.ReadAsStringAsync();
+            //var employee = JsonSerializer.Deserialize<Employee>(responseContent);
+            //return employee;
+
+            // Solution 3: using Newtonsoft.Json; PostAsync
+            var json = JsonConvert.SerializeObject(newEmployee);
+            var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/employees", stringContent);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var employee = JsonConvert.DeserializeObject<Employee>(responseContent);
+            return employee;
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            await httpClient.DeleteAsync($"api/employees/{id}");
+        }
+    }
+}
